41a017568b9957c3be7e02906d0d53a5d8105414
Tariq5mo <tariqomer756@gmail.com>
2026-09-09 11:30:04 +0200

    refactor: restructure configuration to include database migration settings

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,26 @@
+process.loadEnvFile();
+import { MigrationConfig } from "drizzle-orm/migrator";
+import { migrationConfig } from "./drizzle.config.js";
+
 type APIConfig = {
-	fileserverHits: number;
+  fileserverHits: number;
+  port: number;
 };
 
-export const config: APIConfig = {
-	fileserverHits: 0,
+type DBConfig = {
+  dbURL: string;
+  migrationConfig: MigrationConfig;
+};
+
+export const config: APIConfig & DBConfig = {
+  fileserverHits: 0,
+  dbURL: envOrThrow("DB_URL"),
+  migrationConfig: migrationConfig,
+  port: Number(envOrThrow("PORT")),
+};
+
+function envOrThrow(key: string) {
+  const value = process.env[key];
+  if (!value) throw new Error(`Doesn't ${key} exist`);
+  return value;
 }
