96c25c9fd7e43a6c022072169cc8d2a700f38f4c
Tariq5mo <tariqomer756@gmail.com>
2026-09-10 10:46:26 +0200

    feat: add chirp creation and retrieval functions to handle chirp data

---
+++ src/db/queries/chirps.ts
@@ -0,0 +1,51 @@
+import { Request, Response } from "express";
+import { badRequestError } from "../../api/error.js";
+import { db } from "../index.js";
+import { chirps, InsertNewChirpSchema, Newchirp } from "../schema.js";
+import { eq } from "drizzle-orm";
+import { getBearerToken, validateJWT } from "../../api/auth.js";
+import { config } from "../../config.js";
+
+export async function createChirp(req: Request, res: Response) {
+  const parsedBody: Newchirp = req.body;
+  try {
+    const userId = validateJWT(getBearerToken(req), config.api.jwtSecret)
+    const cleanedBody = InsertNewChirpSchema.parse(parsedBody);
+    const [newChirp] = await db
+      .insert(chirps)
+      .values({
+        body: cleanedBody.body,
+        userId: userId,
+      })
+      .returning();
+    return res.status(201).send(newChirp);
+  } catch (error) {
+    throw new badRequestError(`Invalid input ${error}`);
+  }
+}
+
+export async function getAllChirps(req: Request, res: Response) {
+  try {
+    const allChirps = await db.select().from(chirps).orderBy(chirps.createdAt);
+    return res.status(200).send(allChirps);
+  } catch (error) {
+    throw new Error("Failed to retrieves all chirps");
+  }
+}
+
+export async function getOneChirp(req: Request, res: Response) {
+  try {
+    const chirpId = req.params.chirp;
+    const result = await db
+      .select()
+      .from(chirps)
+      .where(eq(chirps.id, Array.isArray(chirpId)? chirpId[0]: chirpId))
+      .limit(1);
+    if (result.length === 0) {
+      return res.status(404).json({ error: "Chirp not found" });
+    }
+    return res.status(200).send(result[0]);
+  } catch (error) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
+}
