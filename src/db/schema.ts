940feb13eb8238aae5bba82f9382b95349e2030d
Tariq5mo <tariqomer756@gmail.com>
2026-09-10 10:46:36 +0200

    feat: add chirp schema and insertion validation with Zod

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,4 +1,5 @@
 import { pgTable, timestamp, varchar, uuid } from "drizzle-orm/pg-core";
+import { createInsertSchema } from "drizzle-zod";
 
 export const users = pgTable("users", {
   id: uuid("id").primaryKey().defaultRandom(),
@@ -8,6 +9,32 @@ export const users = pgTable("users", {
     .defaultNow()
     .$onUpdate(() => new Date()),
   email: varchar("email", { length: 256 }).unique().notNull(),
+  hashedPassword: varchar("hashed_password").notNull(),
+});
+
+export const chirps = pgTable("chirps", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at")
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+  body: varchar("body").notNull(),
+  userId: uuid("user_id")
+    .notNull()
+    .references(() => users.id, { onDelete: "cascade" }),
 });
 
 export type NewUser = typeof users.$inferInsert;
+export type Newchirp = typeof chirps.$inferInsert;
+import { z } from "zod";
+
+export const InsertNewChirpSchema = createInsertSchema(chirps).omit({ userId: true }).extend({
+    token: z.string().nonempty().optional(),
+});
+export const InsertNewUserSchema = createInsertSchema(users)
+  .omit({ hashedPassword: true })
+  .extend({
+    password: z.string().nonempty(),
+    expiresInSeconds: z.string().optional()
+  });
