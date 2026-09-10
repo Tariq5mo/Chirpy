5cac36ddf9b1712229a19ac6a029b27745bb20d0
Tariq5mo <tariqomer756@gmail.com>
2026-09-10 17:42:26 +0200

    fix: update response logging condition to capture all non-2xx status codes

--- src/api/middleware.ts
+++ src/api/middleware.ts
@@ -13,7 +13,7 @@ export function middlewareLogResponses(
   next: NextFunction,
 ) {
   res.on("finish", () => {
-    if (res.statusCode !== 200) {
+    if (res.statusCode > 299) {
       console.log(
         `[NON-OK] ${req.method} ${req.url} - Status: ${res.statusCode}`,
       );
