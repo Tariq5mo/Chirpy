416724074f10153f37da9ea1090ef9bc8bd216d4
Tariq5mo <tariqomer756@gmail.com>
2026-09-10 10:45:58 +0200

    feat: enhance user registration and login endpoints with JWT support

--- src/index.ts
+++ src/index.ts
@@ -12,58 +12,82 @@ import { migrate } from "drizzle-orm/postgres-js/migrator";
 import { drizzle } from "drizzle-orm/postgres-js";
 import { handlerMetrics } from "./api/metrics.js";
 import { db } from "./db/index.js";
-import { users } from "./db/schema.js";
+import {
+  InsertNewUserSchema,
+  NewUser,
+  users,
+} from "./db/schema.js";
+import { createChirp, getAllChirps, getOneChirp } from "./db/queries/chirps.js";
+import { checkPasswordHash, getBearerToken, hashPassword, makeJWT, validateJWT } from "./api/auth.js";
+import { eq } from "drizzle-orm";
 
 const migrationClient = postgres(config.db.url, { max: 1 });
 await migrate(drizzle(migrationClient), config.db.migrationConfig);
 
 const app: Express = express();
 
-const bannedWords = ["kerfuffle", "sharbert", "fornax"];
-
-const bannedWordsRegex = new RegExp(bannedWords.join("|"), "gi");
-
-const bodyResSchema = z.object({
-  body: z
-    .string()
-    .max(140, { message: "Chirp is too long" })
-    .transform((val) => {
-      // Replace all matching words with asterisks
-      return val.replace(bannedWordsRegex, "****");
-    }),
-});
-
-type bodyResType = z.infer<typeof bodyResSchema>;
-
 app.use(middlewareLogResponses, express.json());
 
 app.use("/app", middlewareMetricsInc, express.static("./src/app"));
 
 app.post("/api/users", async (req: Request, res: Response) => {
-  const parsedBody: { email: string } = req.body;
-
   try {
+    const parsedBody = InsertNewUserSchema.parse(req.body);
     const [newUser] = await db
       .insert(users)
       .values({
         email: parsedBody.email,
+        hashedPassword: await hashPassword(parsedBody.password),
       })
       .returning();
-    return res.status(201).send(newUser);
+    type NewUserClean = Omit<NewUser, "hashedPassword">;
+    const newUserClean: NewUserClean = {
+      id: newUser.id,
+      email: newUser.email,
+      createdAt: newUser.createdAt,
+      updatedAt: newUser.updatedAt,
+    };
+    return res.status(201).send(newUserClean);
   } catch (error) {
     throw new badRequestError("Invalid input");
   }
 });
 
-app.post("/api/validate_chirp", (req: Request, res: Response) => {
-  const parsedBody: bodyResType = req.body;
+app.post("/api/login", async (req: Request, res: Response) => {
   try {
-    const cleanedBody = bodyResSchema.parse(parsedBody);
-    return res.send({ cleanedBody: cleanedBody.body });
+    const parsedBody = InsertNewUserSchema.parse(req.body);
+    const [result] = await db
+      .select()
+      .from(users)
+      .where(eq(users.email, parsedBody.email))
+      .limit(1);
+    if (!result) {
+      return res.status(401).send();
+    }
+    const valid = await checkPasswordHash(
+      parsedBody.password,
+      result.hashedPassword,
+    );
+    if (valid) {
+      const token = makeJWT(result.id, parsedBody.expiresInSeconds && Number(parsedBody.expiresInSeconds) < 3600 ? Number(parsedBody.expiresInSeconds) : 3600, config.api.jwtSecret)
+
+      const newUserClean = {
+        id: result.id,
+        email: result.email,
+        createdAt: result.createdAt,
+        updatedAt: result.updatedAt,
+        token: token
+      };
+      return res.status(200).send(newUserClean);
+    } else return res.status(401).send();
   } catch (error) {
-    throw new badRequestError("Chirp is too long. Max length is 140");
+    throw new badRequestError("Invalid input");
   }
 });
+
+app.post("/api/chirps", createChirp);
+app.get("/api/chirps", getAllChirps);
+app.get("/api/chirps/:chirp", getOneChirp);
 
 app.get("/api/healthz", (req: Request, res: Response) => {
   res.set({
@@ -76,7 +100,7 @@ app.get("/api/healthz", (req: Request, res: Response) => {
 app.get("/admin/metrics", handlerMetrics);
 
 app.post("/admin/reset", async (req: Request, res: Response) => {
-  if (config.api.platform !== "dev") return res.status(403).send()
+  if (config.api.platform !== "dev") return res.status(403).send();
   await db.delete(users);
   res.set({
     "Content-Type": "text/plain",
